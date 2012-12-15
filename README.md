43ec7876c1aa2311e8ce63b85dfb2e4a9664b6a8
Frank Usrs <frankusrs@gmail.com>
2012-12-15 11:41:31 +0100

    Pretty much rewrote the whole thing to use OOP. Also added buffering and a handler which can be used in conjunction with webserver configs to serv CGI without creating silly wrapper scripts.

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ PHP CGI Wrapper
 About
 -----
 
-PHP CGI Wrapper is a PHP script made to run CGI applications on hosts
-which don't support CGI. Something similar has been created previously
-(see http://www.fun2code.de/articles/wrapping_perl_with_php/wrapping_perl_with_php.html),
-but that lacked support for file uploads thanks to PHP's **brilliant**
-idea of disabling access to raw POST data when using multipart/form-data
-encoding. This wrapper gets around that problem by reconstructing the POST
-data and feeding it to the application's STDIN.
+PHP CGI Wrapper is a PHP script made to run CGI applications on hosts which
+don't support CGI. Something similar has been created previously (see
+http://www.fun2code.de/articles/wrapping_perl_with_php/wrapping_perl_with_php.html),
+but that lacked support for file uploads thanks to PHP's **brilliant** idea of
+disabling access to raw POST data when using multipart/form-data encoding. This
+wrapper gets around that problem by reconstructing the POST data and feeding it
+to the application's STDIN.
 
 Usage
 -----
@@ -19,46 +19,21 @@ Create a PHP file for every CGI application you want to wrap using PHP. It
 should look like this:
 
 	<?php
-		require 'cgi_wrapper.php';
-		execute_cgi('/path/to/your/cgi/application.pl');
-	?>
+	require 'cgi_wrapper.php';
+	execute_cgi('/path/to/your/cgi/application.pl');
 
 The argument of `execute_cgi()` can be either a relative path
-(`./some-script.pl`), an absolute path (like in the example above) or a
-shell command (`python cgi-script.py`). In the first two cases, the
-script will most likely have to be executable by the web server.
+(`./some-script.pl`), an absolute path (like in the example above) or a shell
+command (`python cgi-script.py`). In the first two cases, the script will most
+likely have to be executable by the web server.
 
-Security concerns
------------------
-
-Be sure that the CGI applications themselves aren't directly accessible.
-If they are, people could obtain passwords and other stuff inside your
-scripts.
-
-Caveats
--------
-
-* I've only tested this with Wakaba (one of the best CGI scripts by far)
-  and a few crappy scripts I made for testing purposes. I'm confident that
-  99% of all working CGI applications should be able to use this without
-  problems, though.
-
-* No error handling at the moment. This will be rectified.
-
-* If you plan on using this script for running CGI scripts on your webhost,
-  it is extremely likely that your host will lack modules or drivers needed
-  for your script. With Perl, this typically means DBI, URI::Escape and
-  similar.
-
-* I haven't done performance checks, because the system I developed this on
-  actually runs PHP as CGI. However, performance isn't much of a concern
-  with CGI -- if you want speed, FastCGI or web server modules such as
-  mod_perl is the way to go.
+See https://github.com/frankusrs/PHP-CGI-Wrapper/wiki for other deployment
+options, security considerations and so on.
 
 Licence
 -------
 
-This script is licenced under the Do What The Fuck You Want To Public
-License for maximum freedom. See the terms and conditions here:
+This script is licenced under the Do What The Fuck You Want To Public License
+for maximum freedom. See the terms and conditions here:
 
 http://sam.zoy.org/wtfpl/COPYING
--- cgi_wrapper.php
+++ cgi_wrapper.php
@@ -1,98 +1,220 @@
 <?php
-	/*
-	 * PHP CGI Wrapper
-	 *     by Frank Usrs
-	 *
-	 * This script is a wrapper for running CGI scripts through PHP. For
-	 * help and support, consult the included README file and/or visit
-	 * the GitHub project page:
-	 *
-	 * https://github.com/frankusrs/PHP-CGI-Wrapper
-	 *
-	 * This program is free software. It comes without any warranty, to
-	 * the extent permitted by applicable law. You can redistribute it
-	 * and/or modify it under the terms of the Do What The Fuck You Want
-	 * To Public License, Version 2, as published by Sam Hocevar. See
-	 * http://sam.zoy.org/wtfpl/COPYING for more details.
-	 */
 
-	ini_set('display_errors', true);
+/*
+ * PHP CGI Wrapper
+ *     by Frank Usrs
+ *
+ * This script is a wrapper for running CGI scripts through PHP. For
+ * help and support, consult the included README file and/or visit
+ * the GitHub project page:
+ *
+ * https://github.com/frankusrs/PHP-CGI-Wrapper
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
 
-	define('MULTIPART_FORMAT', "%s\r\nContent-Disposition: form-data; name=\"%s\"");
-	define('MULTIPART_TEXT_FORMAT', MULTIPART_FORMAT . "\r\n\r\n%s\r\n");
-	define('MULTIPART_FILE_FORMAT', MULTIPART_FORMAT . "; filename=\"%s\"\r\nContent-Type: %s\r\n\r\n%s\r\n");
+ini_set('display_errors', true);
+error_reporting(E_ALL & ~E_STRICT);
 
-	function escapequotes(&$string) {
-		$string = str_replace('"', '\"', $string);
+// Defined here because we can't concatenate class constants.
+define('MULTIPART_FORMAT', "%s\r\nContent-Disposition: form-data; name=\"%s\"");
+define('MULTIPART_TEXT_FORMAT', MULTIPART_FORMAT."\r\n\r\n%s\r\n");
+define('MULTIPART_FILE_FORMAT',
+	MULTIPART_FORMAT."; filename=\"%s\"\r\nContent-Type: %s\r\n\r\n");
+
+class PHP_CGI_Wrapper {
+	const BUFSIZ = 1024;
+
+	protected $script, $ph, $pipes;
+	public function __construct($script) {
+		$this->script		= $script;
+
+		$this->cookie		= &$_COOKIE;
+		$this->env		= &$_ENV;
+		$this->files		= &$_FILES;
+		$this->get		= &$_GET;
+		$this->post		= &$_POST;
+		$this->server		= &$_SERVER;
+
+		$this->content_type	= $_SERVER['CONTENT_TYPE'];
+		$this->request_method	= $_SERVER['REQUEST_METHOD'];
 	}
 
-	function execute_cgi($script)
-	{
+	public function run() {
+		$this->fix_env();
+		$this->set_cookies();
+
+		$this->ph = proc_open($this->script, array(
+			array('pipe', 'rb'), // STDIN
+			array('pipe', 'wb'), // STDOUT
+			// STDERR - TODO
+		), $this->pipes);
+
+		$this->handle_post();
+
+		$this->do_headers();
+		$this->do_body();
+
+		$this->close();
+	}
+
+	protected function fix_env() {
 		// Set the proper environment variables if they aren't present.
-		if (!isset($_ENV['REQUEST_METHOD']))
-			foreach ($_SERVER as $var => $value)
-				putenv("$var=$value"); // not sure if safe
+		if (!isset($this->env['REQUEST_METHOD'])) {
+			foreach ($this->server as $var => $value) {
+				putenv($var.'='.$value); // not sure if safe
+			}
+		}
+	}
+
+	protected function set_cookies() {
+		if (isset($this->env['HTTP_COOKIE']) || !count($this->cookie))
+			return; // cookies already set or none available
 
 		// Set cookies
-		if (!isset($_ENV['HTTP_COOKIE']) && count($_COOKIE)) {
-			$cookies = array();
-			foreach ($_COOKIE as $name => $value)
-				$cookies[] = urlencode($name).'='.urlencode($value);
+		$cookies = array();
+		foreach ($this->cookie as $name => $value)
+			$cookies[] = urlencode($name).'='.urlencode($value);
 
-			putenv(sprintf('HTTP_COOKIE=%s', implode('; ', $cookies)));
+		putenv('HTTP_COOKIE='.implode('; ', $cookies));
+	}
+
+	protected $boundary;
+	const MULTIPART_RE = '!^multipart/form-data; boundary=([^\s]+)!';
+
+	protected function is_multipart() {
+		if (preg_match(self::MULTIPART_RE, $this->content_type, $m)) {
+			$this->boundary = '--'.$m[1];
+			return true;
 		}
 
-		$ph = proc_open($script, array(
-			array('pipe', 'r'), // STDIN
-			array('pipe', 'w'), // STDOUT
-		), $pipes);
+		return false;
+	}
 
-		if ($_SERVER['REQUEST_METHOD'] == "POST") {
-			/* Because PHP is a steaming pile of shit, you can't access the raw post data
-			 * when using multipart/form-data encoding. Thus, we waste our PRECIOUS CPU
-			 * CYCLES reconstructing the POST data manually. */
-			if (preg_match('!^multipart/form-data; boundary=([^\s]+)!', $_SERVER['CONTENT_TYPE'], $matches)) {
-				$boundary = '--' . $matches[1];
-
-				foreach ($_POST as $name => $value) {
-					escapequotes($name);
-					fwrite($pipes[0], sprintf(MULTIPART_TEXT_FORMAT, $boundary, $name, $value));
-				}
-
-				foreach ($_FILES as $name => $file) {
-					$error = $file['error'] === UPLOAD_ERR_OK ? false : true;
-					$type = $error ? 'application/octet-stream' : $file['type'];
-					$filename = $file['name'];
-
-					escapequotes($name);
-					escapequotes($filename);
-
-					// maybe we should use some sort of buffer here instead of grabbing the whole file at once?
-					fwrite($pipes[0], sprintf(MULTIPART_FILE_FORMAT, $boundary, $name, $filename, $type,
-						$error ? '' : file_get_contents($file['tmp_name'])));
-				}
-
-				fwrite($pipes[0], "$boundary--\r\n");
+	protected function handle_post() {
+		if ($this->request_method === 'POST') {
+			if ($this->is_multipart()) {
+				$this->handle_multipart();
 			} else {
-				// URL-encoded POST data
-				fwrite($pipes[0], file_get_contents('php://input'));
+				$this->handle_urlencoded();
 			}
 		}
 
-		fclose($pipes[0]);
+		// we need to close STDIN regardless of whether we use it or not
+		fclose($this->pipes[0]);
+	}
 
-		// headers
-		while (($line = fgets($pipes[1])) !== false && $line != "\n" && $line != "\r\n")
-			header($line, false);
+	protected function handle_urlencoded() {
+		/* send the unmodified POST data to STDIN */
+		$fh = fopen('php://input', 'rb');
+		while (($buf = fgets($fh, self::BUFSIZ)) !== false) {
+			fwrite($this->pipes[0], $buf);
+		}
+		fclose($fh);
+	}
 
-		// body
-		while (($line = fgets($pipes[1])) !== false)
-			echo $line;
+	/* Because PHP is a steaming pile of shit, you can't access the raw post
+	 * data when using multipart/form-data encoding. Thus, we waste our
+	 * PRECIOUS CPU CYCLES reconstructing the POST data manually. */
+	protected function handle_multipart() {
+		// do regular POST values
+		foreach ($this->post as $name => $value) {
+			$name = $this->escapequotes($name);
+			fwrite($this->pipes[0], sprintf(MULTIPART_TEXT_FORMAT,
+				$this->boundary, $name, $value));
+		}
 
-		// and we're done
-		fclose($pipes[1]);
-		proc_close($ph);
+		// handle files uploads
+		$this->handle_files();
 
+		// end of POST data
+		fwrite($this->pipes[0], $this->boundary."--\r\n");
+	}
+
+	const DEFAULT_TYPE = 'application/octet-stream';
+	protected function handle_files() {
+		foreach ($this->files as $name => $file) {
+			// should we use is_uploaded_file() here? only
+			// register_globals could warrant that, and it's gone as
+			// of php 5.4 anyway
+
+			$error = $file['error'] !== UPLOAD_ERR_OK;
+
+			$type = $error ? self::DEFAULT_TYPE : $file['type'];
+			$filename = $file['name'];
+
+			$name = $this->escapequotes($name);
+			$filename = $this->escapequotes($filename);
+
+			// header for file
+			fwrite($this->pipes[0], sprintf(MULTIPART_FILE_FORMAT,
+				$this->boundary, $name, $filename, $type));
+
+			// write file to STDIN
+			$fh = fopen($file['tmp_name'], 'rb');
+			while (($buf = fgets($fh, self::BUFSIZ)) !== false) {
+				fwrite($this->pipes[0], $buf);
+			}
+			fclose($fh);
+
+			// end of file
+			fwrite($this->pipes[0], "\r\n");
+		}
+	}
+
+	public function do_headers() {
+		while (1) {
+			while (($line = fgets($this->pipes[1])) !== false) {
+				if (preg_match('/^(?:\r?\n|\r)$/', $line))
+					break 2; // End of headers
+
+				header($line, false);
+			}
+
+			// Premature end of headers - TODO
+			die();
+		}
+	}
+
+	public function do_body() {
+		while (($buf = fgets($this->pipes[1], self::BUFSIZ)) !== false)
+			echo $buf;
+	}
+
+	public function close() {
+		fclose($this->pipes[1]);
+		proc_close($this->ph);
+	}
+
+	protected static function escapequotes($string) {
+		return str_replace('"', '\"', $string);
+	}
+
+	const CGI_FILENAME = 'DOCUMENT_PATH';
+	public static function is_handler() {
+		return isset($_SERVER[self::CGI_FILENAME]);
+	}
+	
+	public static function do_handler() {
+		chdir(dirname($_SERVER[self::CGI_FILENAME]));
+		execute_cgi(escapeshellarg($_SERVER[self::CGI_FILENAME]));
 		exit;
 	}
-?>
+}
+
+function execute_cgi($script) {
+	$cgi = new PHP_CGI_Wrapper($script);
+	$cgi->run();
+
+	exit;
+}
+
+// If $_SERVER['DOCUMENT_PATH'], a non-standard environment value, is set, then
+// assume we're acting like a handler for CGI scripts in a web server setup.
+if (PHP_CGI_Wrapper::is_handler()) {
+	PHP_CGI_Wrapper::do_handler();
+}
